6626f729e932fac783c5d5698f64f1b6b8f05f18
DonViktorash <vik.markin2014@yandex.ru>
2020-11-21 01:34:45 +0300

    Commit Practise 6

---
+++ Practise 6/server/src/main/java/server/Main.java
@@ -0,0 +1,12 @@
+package server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello,world");
+//        String text = "1,2,3,4,5";
+        String text = "-10,-257,-3.1,-4000,-5";
+        Math math = new Math(text);
+        math.Parser();
+        System.out.println("Result of work is:"+ math.getResult());
+    }
+}
